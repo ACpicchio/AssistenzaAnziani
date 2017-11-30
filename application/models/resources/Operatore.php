59c1995752d90163b31819c0f75f3e3a360628fb
marcociotti <ciotti7mc@gmail.com>
2017-11-30 17:26:46 +0100

    un po di model

---
+++ application/models/resources/Operatore.php
@@ -0,0 +1,27 @@
+<?php
+
+class Application_Resource_Operatore extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'operatore';
+    protected $_primary = 'cf';
+    protected $_rowClass = 'Application_Resource_Operatore_Item';
+
+    public function init()
+    {
+    }
+
+    public function nuovoOperatore($info)
+    {
+        return $this->insert($info);
+    }
+
+    public function getOperatoreByTipo($tipo)
+    {
+        return $this->fetchAll($this->select()->where('professione = ?', $tipo));
+    }
+
+    public function getOperatoreByCitta($citta)
+    {
+        return $this->fetchAll($this->select()->where('citta = ?', $citta));
+    }
+}
