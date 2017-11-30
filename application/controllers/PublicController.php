ddc6c4b7815f460e596149c4c52564c9febb61b0
marcociotti <ciotti7mc@gmail.com>
2017-11-30 13:11:55 +0100

    login

--- application/controllers/PublicController.php
+++ application/controllers/PublicController.php
@@ -7,11 +7,92 @@ class PublicController extends Zend_Controller_Action
 
     public function init()
     {
+
         $this->_helper->layout->setLayout('main');
+        $this->view->loginForm = $this->getLoginForm();
+
     }
 
     public function indexAction()
     {
+    }
+
+    public function registrazioneAction()
+    {
+    }
+
+    public function registratiAction()
+    {
+        if (!$this->getRequest()->isPost()) {
+            $this->_helper->redirector('index');
+        }
+        $form=$this->_registrazioneform;
+        if (!$form->isValid($_POST)) {
+            $form->setDescription('Attenzione: alcuni dati inseriti sono errati.');
+            return $this->render('registrazione');
+        }
+        else {
+            $values = $form->getValues();
+            $values['tipo'] = 'user';
+            if ($this->_userModel->usernameUsato($values['username'])) {        // restituisce vero se esiste già quell'username
+                $form->setDescription('Attenzione: Username non disponibile. Scegline un altro.');
+                return $this->render('registrazione');
+            }
+            else {
+                $this->_userModel->registrazione($values);
+                $this->_helper->redirector('registrazioneeffettuata');}
+
+        }
+    }
+    public function registrazioneeffettuataAction()
+    {
+    }
+
+    public function loginAction()
+    {
+
+    }
+
+    private function getLoginForm()
+    {
+        $urlHelper = $this->_helper->getHelper('url');
+        $this->_loginform = new Application_Form_Public_Auth_Login();
+        $this->_loginform->setAction($urlHelper->url(array(
+            'controller' => 'public',
+            'action' => 'authenticate'),
+            'default'
+        ));
+        return $this->_loginform;
+    }
+
+    private function getRegistrazioneForm()
+    {
+        $urlHelper = $this->_helper->getHelper('url');
+        $this->_registrazioneform = new Application_Form_Public_Auth_Registrazione();
+        $this->_registrazioneform->setAction($urlHelper->url(array(
+            'controller' => 'public',
+            'action' => 'registrati'),
+            'default'
+        ));
+        return $this->_registrazioneform;
+    }
+
+    public function authenticateAction()
+    {
+        $request = $this->getRequest();
+        if (!$request->isPost()) {
+            return $this->_helper->redirector('login');
+        }
+        $form = $this->_loginform;
+        if (!$form->isValid($request->getPost())) {
+            $form->setDescription('Attenzione: alcuni dati inseriti sono errati.');
+            return $this->render('login');
+        }
+        if (false === $this->_authService->authenticate($form->getValues())) {
+            $form->setDescription('Autenticazione fallita. Riprova');
+            return $this->render('login');
+        }
+        return $this->_helper->redirector('index', $this->_authService->getIdentity()->tipo);
     }
 
 }
