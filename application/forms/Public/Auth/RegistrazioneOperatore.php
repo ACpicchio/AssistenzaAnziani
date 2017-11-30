b0c76f3171e4998e1d064be165b834d661321a99
marcociotti <ciotti7mc@gmail.com>
2017-11-30 15:15:11 +0100

    form

---
+++ application/forms/Public/Auth/RegistrazioneOperatore.php
@@ -0,0 +1,70 @@
+<?php
+
+class Application_Form_Public_Auth_RegistrazioneOperatore extends App_Form_Abstract
+{
+    public function init()
+    {
+        $this->setMethod('post');
+        $this->setName('registrazioneOperatore');
+        $this->setAction('');
+
+        $this->addElement('text', 'nome', array(
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                array('StringLength', true, array(2, 25))
+            ),
+            'required'   => true,
+            'label'      => 'Nome',
+            'decorators' => $this->elementDecoratorsReg,
+        ));
+
+        $this->addElement('text', 'cognome', array(
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                array('StringLength', true, array(2, 25))
+            ),
+            'required'   => true,
+            'label'      => 'Cognome',
+            'decorators' => $this->elementDecoratorsReg,
+        ));
+
+
+        $this->addElement('text', 'email', array(
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                array('StringLength', true, array(3, 100))
+            ),
+            'required'   => true,
+            'label'      => 'Email',
+            'decorators' => $this->elementDecoratorsReg,
+        ));
+
+
+
+        $this->addElement('password', 'password', array(
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                array('StringLength', true, array(3, 25))
+            ),
+            'required'   => true,
+            'label'      => 'Password',
+            'decorators' => $this->elementDecoratorsReg,
+        ));
+
+
+        $this->addElement('submit', 'registrati', array(
+
+            'label'      => 'Registrati',
+            'decorators' => $this->buttonDecoratorsReg,
+        ));
+
+
+        $this->setDecorators(array(
+            'FormElements',
+            array('HtmlTag', array('tag' => 'table', 'class' => 'zend_form centered center-align row col s12"')),
+            array('Description', array('placement' => 'prepend', 'class' => 'form-error')),
+            'Form'
+        ));
+
+    }
+}
